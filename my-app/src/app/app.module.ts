27dcefc6d067ad686bb1591966b0f1a93989565f
Kendras <kevin95180@free.fr>
2018-09-24 22:54:44 +0200

    Route

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes, Route } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { HomeComponent } from './home/home.component';
+import { CarteComponent } from './carte/carte.component';
+import { ReserveComponent } from './reserve/reserve.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'carte', component: CarteComponent},
+  {path: 'reserve', component: ReserveComponent},
+  {path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    HomeComponent,
+    CarteComponent,
+    ReserveComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
